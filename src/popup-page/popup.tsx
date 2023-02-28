a98484f3769fe2eeb94c767b75cfdb82a8f0b8b3
Navojith <navojith22@gmail.com>
2023-02-28 16:30:17 +0530

    extension init

---
+++ src/popup-page/popup.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { render } from 'react-dom';
+
+interface IProps {}
+
+export const Popup: FC<IProps> = () => {
+  return <div>popup</div>;
+};
+
+render(<Popup />, document.getElementById('popup'));
